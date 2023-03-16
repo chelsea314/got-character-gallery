a41a842d927bad91a8815f5a4031acb344076ca1
chelsea314 <chelseansexton@gmail.com>
2023-03-16 11:36:54 -0500

    Completed project

--- src/components/CharacterGallery.js
+++ src/components/CharacterGallery.js
@@ -1,15 +1,5 @@
-import React from 'react'
-import characterData from '../characterData.json'
-import Character from '../components/Character'
-
-const CharacterGallery = () => {
-    const characterList = characterData.map(character => <Character {...character} key={character.id} />)
+function CharacterGallery() {
     return (
-        <div
-            className='gallery-container' data-test='char-gallery'>
-            {characterList}
-        </div>
-    );
-};
-
-export default CharacterGallery;
+        
+    )
+}
